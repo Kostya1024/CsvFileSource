7040b8fdbbf11be366b272228bc2b8ef569e073f
Kostya1024 <snaaack@mail.ru>
2022-06-29 21:53:37 +0200

    added project and tests against it

---
+++ src/main/java/ru/netology/stats/Main.java
@@ -0,0 +1,9 @@
+package ru.netology.stats;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        BonusService calculate = new BonusService();
+    }
+}
---
+++ src/test/java/ru/netology/stats/BonusServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.stats;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+public class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/data.csv"})
+    public void testCSV(long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
